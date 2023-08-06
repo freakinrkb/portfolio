e16493a9bd2bcd02f8b85ffe21a14be61b61e021
Raunak Bhardwaj <ra7nak@gmail.com>
2023-08-06 16:53:49 +0530

    node_modules removed

---
+++ port.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["coder", "designer", "developer","student"],
+    loop : true,
+    typeSpeed: 40,
+    backSpeed : 60,
+    backDelay : 1500
+  };
+  
+  var typed = new Typed('.multitext', options);
